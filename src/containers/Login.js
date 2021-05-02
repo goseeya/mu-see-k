5297ef0990a8361581b42c9fd89926935aa0e1fa
goseeya <149542@gmail.com>
2021-05-02 18:06:00 +0200

    Add login form

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,12 +1,47 @@
+import React, { useState } from "react";
+
 const Login = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    alert(`Submitting with Name ${name}`)
+}
+
     return (
       <div className="Login">
         <header className="Login-header">
           Zaloguj
         </header>
-          <div>
-           content here
-          </div>
+        <form onSubmit={handleSubmit}>
+      <label>
+        Nazwa:
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        E-mail:
+        <input
+          type="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        Hasło:
+        <input
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Zaloguj" />
+    </form>
       </div>
     );
   }
