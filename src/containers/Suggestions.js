ad54344fc74ac2f54f626bdfa2b85988eefafb45
goseeya <149542@gmail.com>
2021-06-18 21:42:04 +0200

    Add push notification

--- src/containers/Suggestions.js
+++ src/containers/Suggestions.js
@@ -62,6 +62,41 @@ const Suggestions = () => {
       .catch((err) => {
         console.log(localhostSuggestions?.suggestions);
         console.log(err);
+      //   setLocalhostSuggestions({
+      //     "genre": "rock",
+      //     "location": "Warszawa",
+      //     "formoney": true,
+      //     "suggestions": [
+      //         {
+      //             "userId": 3,
+      //             "name": "Lars Ulrich",
+      //             "sex": "M",
+      //             "instrument": "perkusja",
+      //             "about": "For whom the bell tolls?",
+      //             "mp3": "fwtbt.m4a",
+      //             "inspirations": "Judas Priest",
+      //             "image1": "cliff1.jpg",
+      //             "image2": "cliff2.jpg",
+      //             "image3": null,
+      //             "likesMe": false
+      //         },
+      //        {
+      //             "userId": 4,
+      //             "name": "Kirk Hammet",
+      //             "sex": "M",
+      //             "instrument": "gitara",
+      //             "about": "For whom the bell tolls?",
+      //             "mp3": "fwtbt.m4a",
+      //             "inspirations": "Judas Priest",
+      //             "image1": "cliff1.jpg",
+      //             "image2": "cliff2.jpg",
+      //             "image3": null,
+      //             "likesMe": true
+      //         }
+      //     ]
+      // })
+      // setShowNoMorePeopleToShow(false);
+
       });
   };
 
@@ -124,13 +159,19 @@ const Suggestions = () => {
       .then(function (response) {
         console.log(response);
 
-        if (localhostSuggestions[currentSuggestionIndex].likesMe) {
+        if (localhostSuggestions?.suggestions[currentSuggestionIndex]?.likesMe) {
+          console.log("likeee");
           createNotification();
           getMatches();
         }
       })
       .catch(function (error) {
         console.log(error);
+        // if (localhostSuggestions?.suggestions[currentSuggestionIndex]?.likesMe) {
+        //   console.log("likeee");
+        //   createNotification();
+        //   getMatches();
+        // }
       });
     swype();
   };
